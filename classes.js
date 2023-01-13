72787e397bef04516ade56c2e5eab42793ac2ca7
Silvio Arem <silvio.arem@gmail.com>
2023-01-13 17:44:16 -0300

    run tsc command in terminal

--- classes.js
+++ classes.js
@@ -1,5 +1,20 @@
 "use strict";
-// Classe definindo os acessos das propriedades das classes podemos dizer se ela pode ser acessadas fora da classe ou não.
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+// Classe = definindo os acessos das propriedades das classes podemos dizer se ela pode ser acessadas fora da classe ou não.
 var Data = /** @class */ (function () {
     // dia: number;
     // mes: number;
@@ -18,3 +33,48 @@ var Data = /** @class */ (function () {
 var data = new Data(1, 1, 2023);
 console.log(data.dia);
 var data2 = new Data(1, 1);
+// Modificadores de Acesso
+var Carro = /** @class */ (function () {
+    function Carro(marca, modelo, velocidadeMaxima) {
+        if (velocidadeMaxima === void 0) { velocidadeMaxima = 220; }
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidadeMaxima = velocidadeMaxima;
+        this.velocidadeAtual = 0;
+    }
+    Carro.prototype.alterarVelocidade = function (delta) {
+        var novaVelocidade = this.velocidadeAtual + delta;
+        if (novaVelocidade >= 0 && novaVelocidade <= this.velocidadeMaxima) {
+            this.velocidadeAtual = novaVelocidade;
+        }
+        else {
+            this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0;
+        }
+    };
+    Carro.prototype.acelerar = function () {
+        this.alterarVelocidade(5);
+    };
+    Carro.prototype.frear = function () {
+        this.alterarVelocidade(-5);
+    };
+    return Carro;
+}());
+var carro = new Carro('Chevrolet', 'prisma', 250);
+carro.acelerar();
+// Herança quando uma classe herda os metodos da classe pai e pode ter novos metodos
+var Camaro = /** @class */ (function (_super) {
+    __extends(Camaro, _super);
+    function Camaro() {
+        var _this = _super.call(this, 'Chevrolet', 'Camaro', 500) || this;
+        _this.turbo = false;
+        return _this;
+    }
+    Camaro.prototype.ligarTurbo = function () {
+        this.turbo = true;
+    };
+    return Camaro;
+}(Carro));
+var camaro = new Camaro();
+camaro.acelerar();
+camaro.frear();
+camaro.ligarTurbo();
