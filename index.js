75eba7a5dc98bbdb4a5aa8ad9c9f401e614f0c4a
Silvio Arem <silvio.arem@gmail.com>
2023-01-13 13:35:39 -0300

    commit initial

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+function somar(numero1, numero2) {
+    return numero1 + numero2;
+}
+let meuNumero1 = 10;
+let meuNumero2 = 5;
+let resultado = somar(meuNumero1, meuNumero2);
+console.log(resultado);
---
+++ index.ts
@@ -0,0 +1,9 @@
+function somar(numero1: number, numero2: number) {
+    return numero1 + numero2;
+}
+
+let meuNumero1 = 10;
+let meuNumero2 = 5;
+
+let resultado = somar(meuNumero1, meuNumero2);
+console.log(resultado);
